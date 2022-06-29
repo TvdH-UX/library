907869889e27ca1709d53f991e741e0df7bb849a
TvdH-UX <thomasheuvel7@gmail.com>
2022-06-29 14:52:55 +0200

    Add delete buttons to books

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ function addBookToLibrary(){
   const pages = prompt('How many pages does it have?');
   const read = prompt('Have you read it already?');
   const book = new Book(title, author, pages, read);
+  // Set data attribute to title?
   myLibrary.push(book);
   printBooks(myLibrary);
 }
@@ -41,6 +42,7 @@ function printBooks(array){
   array.forEach(book => {
     // Generate card and add class
     let card = document.createElement('div');
+    card.setAttribute("title", book.title)
     card.classList.add('book-card');
 
     // Generate title 
@@ -59,15 +61,43 @@ function printBooks(array){
     let bookRead = document.createElement('p');
     bookRead.textContent = book.read;
 
+    // Generate read
+    let bookDelete = document.createElement('button');
+    bookDelete.classList.add('delete-book');
+
     // Append card and content
     books.appendChild(card);
+    // Set data attribute to title?
     card.appendChild(bookTitle);
     card.appendChild(bookAuthor);
     card.appendChild(bookPages);
     card.appendChild(bookRead);
+    card.appendChild(bookDelete);
+    getDeleteBtns();
   });
 };
 
 // Button to add book with
 const addBookBtn = document.getElementById("btn-add-book");
 addBookBtn.addEventListener("click", addBookToLibrary);
+
+// Function to get delete buttons
+function getDeleteBtns() {
+  const deleteBookBtns = document.querySelectorAll(".delete-book");
+  deleteBookBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+      // console.log(button.parentElement.getAttribute('title'))
+      const bookTitle = button.parentElement.getAttribute('title');
+      console.log(bookTitle);
+
+      const index = myLibrary.findIndex(e => e.title === bookTitle);
+      console.log(index);
+
+      myLibrary.splice(index, 1);
+
+      printBooks(myLibrary);
+    })
+  });
+}
+
+printBooks(myLibrary);
